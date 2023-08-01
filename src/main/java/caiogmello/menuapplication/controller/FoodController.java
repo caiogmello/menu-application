abc172d342cc9ac4b346bf7a139be87b7de0e4f4
caiogmello <caiogomedemello@gmail.com>
2023-08-01 18:06:00 -0300

    back-end finished

--- src/main/java/caiogmello/menuapplication/controller/FoodController.java
+++ src/main/java/caiogmello/menuapplication/controller/FoodController.java
@@ -16,12 +16,14 @@ public class FoodController {
     @Autowired
     private FoodRepository repository;
 
+    @CrossOrigin(origins = "*", allowCredentials = "*")
     @PostMapping
     public void postFood(@RequestBody FoodRequestDTO data) {
         Food foodData = new Food(data);
         repository.save(foodData);
     }
 
+    @CrossOrigin(origins = "*", allowCredentials = "*")
     @GetMapping
     public List<FoodResponseDTO> getAll() {
         List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
